81fc1cb61a7dc7fb1999a77719b364e8750c4125
Jatin <jatinagrawal2000@gmail.com>
2024-01-15 14:07:11 +0530

    initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+BlogPost.create(title: 'First Post', body: 'This is the content of the first post.')
+BlogPost.create(title: 'Second Post', body: 'Another post with some interesting content.')
+BlogPost.create(title: 'Third Post', body: 'The third post is here, with more content.')
